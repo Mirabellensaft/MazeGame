41695605fe96a8b6d8ea38f5219cc6b52351c9bd
Mirabellensaft <tanja@transfeld.net>
2018-10-01 19:19:40 +0200

    added exceptions

--- maze.py
+++ maze.py
@@ -1,8 +1,18 @@
-import pyb
-import lcd160cr
+try:
+    import pyb
+    import lcd160cr
+except ImportError:
+    pass
+
 from random import randint, randrange
 
-lcd = lcd160cr.LCD160CR('X')
+try:
+    lcd = lcd160cr.LCD160CR('X')
+    accel = pyb.Accel()
+
+except NameError:
+    pass
+
 
 
 def shuffle(liste):
@@ -16,6 +26,8 @@ def shuffle(liste):
     return new_liste
 
 def make_filled_maze(w, h):
+
+    """generates a grid of vertical and horizontal walls"""
 
     visited = []
     CellCenters = []
@@ -37,28 +49,33 @@ def make_filled_maze(w, h):
     for x in range(10, w, 20):
         for y in range(10, h, 20):
             CellCenters.append((x,y))
+    print ('1', x,y)
 
     def walk(x, y):
+
+        """walks through the grid, deleting walls to create a maze."""
+
+        print ('was ich wiisen', x,y)
 
         visited.append((x, y))
 
         NeighborCells = [(x - 20, y), (x, y + 20), (x + 20, y), (x, y - 20)]
         NeighborCells = shuffle(NeighborCells)
-        print (NeighborCells, x, y)
+        #print (NeighborCells, x, y)
 
         besetzteNachbarn = 0
 
         for (xx, yy) in NeighborCells:
-            print("xx, yy", xx, yy)
+            #print("xx, yy", xx, yy)
 
             if len(CellCenters) == len(visited):
                 # print("visitd", len(visited))
                 # print("CellCenters", len(CellCenters))
-                print ("Längen gleich")
+                #print ("Langen gleich")
                 break
 
             elif besetzteNachbarn == 4:
-                print ("Nachbarschaft zu!")
+                #print ("Nachbarschaft zu!")
                 letzterX = visited[-1][0]
                 letzterY = visited[-1][1]
 
@@ -71,12 +88,12 @@ def make_filled_maze(w, h):
 
             elif (xx, yy) not in CellCenters:
                 besetzteNachbarn += 1
-                print ("not on List")
+                #print ("not on List")
                 continue
 
             elif (xx, yy) in visited:
                 besetzteNachbarn += 1
-                print ("already in List")
+                #print ("already in List")
                 continue
 
 
@@ -85,7 +102,7 @@ def make_filled_maze(w, h):
                 try:
                     ver.remove((x+10, y-10, x+10, y+10))
                 except ValueError:
-                    print ("ValueError")
+                    #print ("ValueError")
                     continue
 
             elif xx == x-20:
@@ -93,7 +110,7 @@ def make_filled_maze(w, h):
                     ver.remove((x-10, y-10, x-10, y+10))
                 except ValueError:
                     #print ((x,y), (x-10, y-10, x-10, y+10))
-                    print ("ValueError")
+                    #print ("ValueError")
                     continue
 
             elif yy == y+20:
@@ -102,7 +119,7 @@ def make_filled_maze(w, h):
                     hor.remove((x-10, y+10, x+10, y+10))
                 except ValueError:
                     #print ((x,y), (x-10, y+10, x+10, y+10))
-                    print ("ValueError")
+                    #print ("ValueError")
                     continue
 
             elif yy == y-20:
@@ -110,41 +127,59 @@ def make_filled_maze(w, h):
                 try:
                     hor.remove((x-10, y-10, x+10, y-10))
                 except ValueError:
-                    print ("ValueError")
+                    #print ("ValueError")
                     #print ((x,y), (x-10, y-10, x+10, y-10))
                     continue
 
 
 
-            lcd.erase()
-            for i in ver:
-                lcd.line(i[0], i[1], i[2], i[3])
-
-            for i in hor:
-                lcd.line(i[0], i[1], i[2], i[3])
+            # lcd.erase()
+            # for i in ver:
+            #     lcd.line(i[0], i[1], i[2], i[3])
+            #
+            # for i in hor:
+            #     lcd.line(i[0], i[1], i[2], i[3])
 
             walk(xx, yy)
 
+
+
+
+
+    RandomStartCell = CellCenters[randint(0, len(CellCenters))]
+    walk(RandomStartCell[0], RandomStartCell[1])
+
+    #print ("visited", visited)
+    return RandomStartCell, visited[-11], ver, hor
+
+if __name__ == "__main__":
+    try:
+        fg = lcd.rgb(255, 255, 255)
+        bg = lcd.rgb(0, 0, 0)
+        fill = lcd.rgb(255, 255, 255)
+
+        lcd.set_pen(fg, bg)
+        lcd.erase()
+
+    except NameError:
+        pass
+
+    Xmax = 120
+    Ymax = 140
+
+    a, b, ver, hor = make_filled_maze(Xmax, Ymax)
+
+    try:
+        lcd.erase()
         for i in ver:
             lcd.line(i[0], i[1], i[2], i[3])
 
         for i in hor:
             lcd.line(i[0], i[1], i[2], i[3])
 
-        lcd.line(w, 0, w, h)
-        lcd.line(0, h, w, h)
 
-    RandomStartCell = CellCenters[randint(0, len(CellCenters))]
-    walk(RandomStartCell[0], RandomStartCell[1])
+        lcd.line(Xmax, 0, Xmax, Ymax)
+        lcd.line(0, Ymax, Xmax, Ymax)
 
-    print ("visited", visited)
-
-
-fg = lcd.rgb(255, 255, 255)
-bg = lcd.rgb(0, 0, 0)
-fill = lcd.rgb(255, 255, 255)
-
-lcd.set_pen(fg, bg)
-lcd.erase()
-
-make_filled_maze(120, 140)
+    except NameError:
+        pass
